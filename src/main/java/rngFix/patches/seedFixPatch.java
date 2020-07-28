d59681af6f829bdcd0efe00bf90563d7b1f53f84
nferris165 <51347535+nferris165@users.noreply.github.com>
2020-07-28 18:31:59 -0400

    seed fix
    altered seed generation algorithm to use seed to generate unique seeds per category

--- src/main/java/rngFix/patches/seedFixPatch.java
+++ src/main/java/rngFix/patches/seedFixPatch.java
@@ -1,4 +1,51 @@
 package rngFix.patches;
 
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.random.Random;
+import rngFix.RngFix;
+
+
 public class seedFixPatch {
+    @SpirePatch(
+            clz = AbstractDungeon.class,
+            method = "generateSeeds"
+    )
+
+    public static class generatePatch {
+        public static void Postfix(){
+            RngFix.logger.info("old seeds:\n");
+
+            RngFix.logger.info(AbstractDungeon.monsterRng.random() + "\n");
+            RngFix.logger.info(AbstractDungeon.eventRng.random() + "\n");
+            RngFix.logger.info(AbstractDungeon.merchantRng.random() + "\n");
+            RngFix.logger.info(AbstractDungeon.cardRng.random() + "\n");
+            RngFix.logger.info(AbstractDungeon.treasureRng.random() + "\n");
+            RngFix.logger.info(AbstractDungeon.relicRng.random() + "\n");
+            RngFix.logger.info(AbstractDungeon.potionRng.random() + "\n");
+
+            Random gen = new Random(Settings.seed);
+
+            AbstractDungeon.monsterRng = new Random(gen.random.nextLong());
+            AbstractDungeon.eventRng = new Random(gen.random.nextLong());
+            AbstractDungeon.merchantRng = new Random(gen.random.nextLong());
+            AbstractDungeon.cardRng = new Random(gen.random.nextLong());
+            AbstractDungeon.treasureRng = new Random(gen.random.nextLong());
+            AbstractDungeon.relicRng = new Random(gen.random.nextLong());
+            AbstractDungeon.potionRng = new Random(gen.random.nextLong());
+
+            RngFix.logger.info("new seeds:\n");
+
+            RngFix.logger.info(AbstractDungeon.monsterRng.random() + "\n");
+            RngFix.logger.info(AbstractDungeon.eventRng.random() + "\n");
+            RngFix.logger.info(AbstractDungeon.merchantRng.random() + "\n");
+            RngFix.logger.info(AbstractDungeon.cardRng.random() + "\n");
+            RngFix.logger.info(AbstractDungeon.treasureRng.random() + "\n");
+            RngFix.logger.info(AbstractDungeon.relicRng.random() + "\n");
+            RngFix.logger.info(AbstractDungeon.potionRng.random() + "\n");
+
+        }
+    }
 }
