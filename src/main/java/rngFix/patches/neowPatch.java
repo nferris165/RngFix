3c937d6354923165e6f02ac9e582fa13a9db18e4
nferris165 <51347535+nferris165@users.noreply.github.com>
2020-07-28 21:18:01 -0400

    organized event rng

--- src/main/java/rngFix/patches/neowPatch.java
+++ src/main/java/rngFix/patches/neowPatch.java
@@ -4,6 +4,7 @@ import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.neow.NeowEvent;
 import com.megacrit.cardcrawl.random.Random;
 import com.megacrit.cardcrawl.rooms.EventRoom;
 import javassist.CannotCompileException;
@@ -11,27 +12,26 @@ import javassist.CtBehavior;
 
 import java.util.ArrayList;
 
-public class eventRngPatch {
-
+public class neowPatch {
     @SpirePatch(
-            clz = EventRoom.class,
-            method = "onPlayerEntry"
+            clz = NeowEvent.class,
+            method = "blessing"
     )
 
-    public static class eventPatch {
+    public static class generatePatch {
         @SpireInsertPatch(
-                locator = Locator.class,
-                localvars = {"eventRngDuplicate"}
+                locator = eventRngPatch.Locator.class,
+                localvars = {"rng"}
         )
-        public static void Insert(EventRoom __instance, @ByRef Random[] eventRngDuplicate){
-            Random gen = new Random(Settings.seed, 1);
-            eventRngDuplicate[0] = new Random(gen.random.nextLong(), AbstractDungeon.eventRng.counter);
+        public static void Insert(EventRoom __instance, @ByRef Random[] rng){
+            Random gen = new Random(Settings.seed, 13);
+            rng[0] = new Random(gen.random.nextLong(), AbstractDungeon.eventRng.counter);
         }
     }
 
     public static class Locator extends SpireInsertLocator {
         public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
-            Matcher finalMatcher = new Matcher.FieldAccessMatcher(EventRoom.class, "event");
+            Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractDungeon.class, "bossCount");
             return LineFinder.findInOrder(ctMethodToPatch, new ArrayList<>(), finalMatcher);
         }
     }
--- src/main/java/rngFix/patches/seedFixPatch.java
+++ src/main/java/rngFix/patches/seedFixPatch.java
@@ -5,8 +5,8 @@ import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.EventHelper;
 import com.megacrit.cardcrawl.random.Random;
-import com.megacrit.cardcrawl.rooms.EventRoom;
 import com.megacrit.cardcrawl.saveAndContinue.SaveFile;
 import javassist.CannotCompileException;
 import javassist.CtBehavior;
@@ -29,19 +29,20 @@ public class seedFixPatch {
             RngFix.logger.info("Generating unique seeds..."); // seed: 7 proves uniqueness
             Random gen = new Random(Settings.seed);
 
-            AbstractDungeon.monsterRng = new Random(gen.random.nextLong());
-            AbstractDungeon.eventRng = new Random(gen.random.nextLong());
-            AbstractDungeon.merchantRng = new Random(gen.random.nextLong());
-            AbstractDungeon.cardRng = new Random(gen.random.nextLong());
-            AbstractDungeon.treasureRng = new Random(gen.random.nextLong());
-            AbstractDungeon.relicRng = new Random(gen.random.nextLong());
-            AbstractDungeon.potionRng = new Random(gen.random.nextLong());
 
-            AbstractDungeon.monsterHpRng = new Random(gen.random.nextLong());
-            AbstractDungeon.aiRng = new Random(gen.random.nextLong());
-            AbstractDungeon.shuffleRng = new Random(gen.random.nextLong());
-            AbstractDungeon.cardRandomRng = new Random(gen.random.nextLong());
-            AbstractDungeon.miscRng = new Random(gen.random.nextLong());
+            AbstractDungeon.monsterRng = new Random(gen.random.nextLong());     // 0
+            AbstractDungeon.eventRng = new Random(gen.random.nextLong());       // 1
+            AbstractDungeon.merchantRng = new Random(gen.random.nextLong());    // 2
+            AbstractDungeon.cardRng = new Random(gen.random.nextLong());        // 3
+            AbstractDungeon.treasureRng = new Random(gen.random.nextLong());    // 4
+            AbstractDungeon.relicRng = new Random(gen.random.nextLong());       // 5
+            AbstractDungeon.potionRng = new Random(gen.random.nextLong());      // 6
+
+            AbstractDungeon.monsterHpRng = new Random(gen.random.nextLong());   // 7
+            AbstractDungeon.aiRng = new Random(gen.random.nextLong());          // 8
+            AbstractDungeon.shuffleRng = new Random(gen.random.nextLong());     // 9
+            AbstractDungeon.cardRandomRng = new Random(gen.random.nextLong());  // 10
+            AbstractDungeon.miscRng = new Random(gen.random.nextLong());        // 11
 
             /*
             RngFix.logger.info("new seeds:\n");
@@ -110,11 +111,28 @@ public class seedFixPatch {
             AbstractDungeon.cardRandomRng = new Random(gen.random.nextLong() + (long)floorNum);
             AbstractDungeon.miscRng = new Random(gen.random.nextLong() + (long)floorNum);
         }
+
+        @SpireInsertPatch(
+                locator = Locator2.class,
+                localvars = {"eventRngDuplicate"}
+        )
+        public static void Insert2(AbstractDungeon __instance, SaveFile save, @ByRef Random[] eventRngDuplicate){
+
+            Random gen = new Random(Settings.seed, 1);
+            eventRngDuplicate[0] = new Random(gen.random.nextLong(), AbstractDungeon.eventRng.counter);
+        }
     }
 
     public static class Locator extends SpireInsertLocator {
         public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
             Matcher finalMatcher = new Matcher.FieldAccessMatcher(SaveFile.class, "post_combat");
+            return LineFinder.findInOrder(ctMethodToPatch, new ArrayList<>(), finalMatcher);
+        }
+    }
+
+    public static class Locator2 extends SpireInsertLocator {
+        public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+            Matcher finalMatcher = new Matcher.MethodCallMatcher(EventHelper.class, "roll");
             return LineFinder.findInOrder(ctMethodToPatch, new ArrayList<>(), finalMatcher);
         }
     }
